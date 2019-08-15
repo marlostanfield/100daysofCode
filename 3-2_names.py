790a6534a2fc0216fb9357dc7d231dcaf0bf9658
Dana <martinez.dbethel@gmail.com>
2019-08-15 07:24:09 -0400

    Delete 3-2_names.py

--- 3-2_names.py
+++
@@ -1,9 +0,0 @@
-names = ['Black Noir' , 'Billy Butcher' , "Homelander" , "A-Train"]
-message = names[0].title() + " The Boys has been renewed for Season 2!!"
-message1 = names[1].title() + " The Boys has been renewed for Season 2!!"
-message2 = names[2].title() + " The Boys has been renewed for Season 2!!"
-message3 = names[3].title() + " The Boys has been renewed for Season 2!!"
-print(message)
-print(message1)
-print(message2)
-print(message3)
