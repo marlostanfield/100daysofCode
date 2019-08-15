867462d7d722e296c2a3280cc59130183e3573fe
Dana <martinez.dbethel@gmail.com>
2019-08-15 07:25:26 -0400

    Delete favorite_number.py

--- favorite_number.py
+++
@@ -1,5 +0,0 @@
-#August 1st, 2019.
-# This program stores my favorite number a as variable then prints it into a statement using the str() function.
-favorite_number = 711
-message = "My favorite number is " + str(favorite_number) + " because it's lucky as hell!!!"
-print(message)
