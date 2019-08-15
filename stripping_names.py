dbf7ab03f2dadfa5d410cb4f1bdec37358e01142
Dana <martinez.dbethel@gmail.com>
2019-08-15 07:26:43 -0400

    Delete stripping_names.py

--- stripping_names.py
+++
@@ -1,8 +0,0 @@
-name = "\n Jason"
-middle_name = "\nDa Man  "
-last_name = "\n\tBourne  "
-full_name = name + " " + middle_name + " " + last_name
-print(full_name)
-print(full_name.rstrip())
-print(full_name.lstrip())
-print(full_name.strip())
